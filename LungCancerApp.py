983af39fc3a5887859e4b6115792a19ab028ca4c
Abilehita <azamegbesarah@gmail.com>
2022-09-27 12:30:27 +0100

    changed the python and pickle files

--- LungCancerApp.py
+++ LungCancerApp.py
@@ -20,17 +20,21 @@ classifier = pickle.load(pickle_in1)
 # In[3]:
 
 
-def prediction(Obesity,Coughing_of_Blood,Passive_smoker,Alcohol_use,Balanced_diet,Wheezing,Dust_allergy,Swallowing_difficulty,Fatigue,Shortness_of_breath,Genetic_risk,Chest_pain,Air_pollution,Snoring,Chronic_lung_disease):
-    dic = {'Obesity':[Obesity],'Coughing_of_Blood':[Coughing_of_Blood],'Passive_smoker':[Passive_smoker],'Alcohol_use':[Alcohol_use],'Balanced_diet':[Balanced_diet],'Wheezing':[Wheezing],'Dust_allergy':[Dust_allergy],'Swallowing_difficulty':[Swallowing_difficulty],'Fatigue':[Fatigue],'Shortness_of_breath':[Shortness_of_breath],'Genetic_risk':[Genetic_risk],'Chest_pain':[Chest_pain],'Air_pollution':[Air_pollution],'Snoring':[Snoring],'Chronic_lung_disease':[Chronic_lung_disease]}
-    df = pd.DataFrame(dic)
-    pca = pickle.load(open("pca.pkl","rb"))
+def prediction(Obesity,Coughing_of_Blood,Genetic_risk,Passive_smoker,Balanced_diet,Swallowing_difficulty,Wheezing,Dust_allergy,Fatigue,Alcohol_use,Shortness_of_breath,Chronic_lung_disease,Chest_pain,Frequent_cold,Snoring):
+    if Genetic_risk == "Yes":
+        Genetic_risk = 1
+    else:
+        Genetic_risk = 0
+    dic = {'Obesity':[Obesity], 'Coughing_of_Blood':[Coughing_of_Blood], 'Genetic_risk':[Genetic_risk], 'Passive_smoker':[Passive_smoker], 'Balanced_diet':[Balanced_diet], 'Swallowing_difficulty':[Swallowing_difficulty], 'Wheezing':[Wheezing], 'Dust_allergy':[Dust_allergy], 'Fatigue':[Fatigue], 'Alcohol_use':[Alcohol_use], 'Shortness_of_breath':[Shortness_of_breath], 'Chronic_lung_disease':[Chronic_lung_disease], 'Chest_pain':[Chest_pain], 'Frequent_cold':[Frequent_cold], 'Snoring':[Snoring]}
+    df = pd.DataFrame(dic) #creating the dataframe
+    pca = pickle.load(open("pca.pkl","rb")) #transforming the dataframe using PCA
     processed = pca.transform(df)
     #making predictions
     prediction = classifier.predict(processed)
     return prediction
 
 
-# In[7]:
+# In[4]:
 
 
 def main():
@@ -43,28 +47,28 @@ def main():
     padding = 0 #reducing the padding
     st.markdown(f""" <style> .reportview-container .main .block-container{{padding-top: {padding}rem; padding-right: {padding}rem; padding-left: {padding}rem; padding-bottom: {padding}rem;}} </style> """, unsafe_allow_html=True)
     #creating the input tabs
-    Obesity = st.slider('How obese are you?',min_value=1, max_value=7,step=1)
-    Coughing_of_Blood = st.slider('Have you ever coughed blood? How often does this happen?',min_value=1, max_value=9,step=1)
-    Passive_smoker = st.slider("How exposed would you say you are to other people's tobacco smoke?",min_value=1, max_value=8,step=1)
+    Obesity = st.slider('Are you obese? If yes, how obese are you?',min_value=1, max_value=7,step=1)
+    Coughing_of_Blood = st.slider('Have you ever coughed blood? If yes, how much blood? Rate it.',min_value=1, max_value=9,step=1)
+    Passive_smoker = st.slider("How often are you exposed to other people's smoke?",min_value=1, max_value=8,step=1)
     Alcohol_use = st.slider("What's your alcohol intake like? Rate it on a scale of 1-8.",min_value=1, max_value=8,step=1)
-    Balanced_diet = st.slider('How balanced is your diet',min_value=1, max_value=7,step=1)
-    Wheezing = st.slider('Do you wheeze a lot? How severe would you say it is?',min_value=1, max_value=8,step=1)
-    Dust_allergy = st.slider('Are you allergic to dust? If so, how severe is your allergy?',min_value=1, max_value=8,step=1)
-    Swallowing_difficulty = st.slider('Any difficulty with swallowing? Rate it.',min_value=1, max_value=8,step=1)
-    Fatigue = st.slider('How fatigued would you say you get?',min_value=1, max_value=9,step=1)
-    Shortness_of_breath = st.slider('Do you experience shortness of breath? If so, how often/severe?',min_value=1, max_value=9,step=1)
-    Genetic_risk = st.slider('Can you rate your genetic risk of lung cancer? You may need to carry out genetic testing for this.',min_value=1, max_value=7,step=1)
-    Chest_pain = st.slider('Do you experience chest pain? How severe is it?',min_value=1, max_value=9,step=1)
-    Air_pollution = st.slider('How polluted is the air you breathe?',min_value=1, max_value=8,step=1)
-    Snoring = st.slider("How severe is your snoring? You may ask living partners or family members for their input.",min_value=1, max_value=7,step=1)
-    Chronic_lung_disease = st.slider('Do you have any form of chronic lung disease? If so, how severe is it?',min_value=1, max_value=7,step=1)
-    result="" #empty string
+    Balanced_diet = st.slider('How balanced is your diet on a scale of 1-7?',min_value=1, max_value=7,step=1)
+    Wheezing = st.slider('How often do you wheeze? And how severe would you say it is?',min_value=1, max_value=8,step=1)
+    Dust_allergy = st.slider('Rate your level of exposure to dust on a scale of 1-8.',min_value=1, max_value=8,step=1)
+    Swallowing_difficulty = st.slider('How often do you find it difficult to swallow? And how severe is it?',min_value=1, max_value=8,step=1)
+    Fatigue = st.slider('How often do you experience fatigue?',min_value=1, max_value=9,step=1)
+    Shortness_of_breath = st.slider('Have you ever been short of breath? Rate your experience based on severity and frequency of occurrence.',min_value=1, max_value=9,step=1)
+    Genetic_risk = st.selectbox('Do you have family history of lung cancer?',("Yes","No"), index=1)
+    Chest_pain = st.slider('Do you experience chest pain? If yes, how painful was the last occurrence?',min_value=1, max_value=9,step=1)
+    Frequent_cold = st.slider('How often do you catch a cold?',min_value=1, max_value=7,step=1)
+    Snoring = st.slider("How loudly do you snore? Inquire from living partners or family members.",min_value=1, max_value=7,step=1)
+    Chronic_lung_disease = st.slider('How often do you experience chronic lung diseases (e.g. asthma, bronchitis, pneumonia, etc.)',min_value=1, max_value=7,step=1)
+    
     #creating the predict button and output message
     if st.button("Predict"): 
-        result = prediction(Obesity,Coughing_of_Blood,Passive_smoker,Alcohol_use,Balanced_diet,Wheezing,Dust_allergy,Swallowing_difficulty,Fatigue,Shortness_of_breath,Genetic_risk,Chest_pain,Air_pollution,Snoring,Chronic_lung_disease)
-        if result[0] == 0:
+        result = prediction(Obesity,Coughing_of_Blood,Genetic_risk,Passive_smoker,Balanced_diet,Swallowing_difficulty,Wheezing,Dust_allergy,Fatigue,Alcohol_use,Shortness_of_breath,Chronic_lung_disease,Chest_pain,Frequent_cold,Snoring)
+        if result == 0:
             st.success("Your risk level is low.") 
-        elif result[0] == 1:
+        elif result == 1:
             st.success("Your risk level is medium.")
         else:
             st.success("Your risk level is high!")
